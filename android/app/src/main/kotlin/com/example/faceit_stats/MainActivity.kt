d2f752ab96087483170a82902f5c70006ecc4a39
Jackie <Jack3459@stud.kea.dk>
2021-11-25 10:35:00 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/faceit_stats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.faceit_stats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// Sizes
+const kDefaultPadding = 20.0;
+const kDefaultWidth = 56.0;
+
+// Colors
+const kPrimaryOrange = Color(0xFFFF5500);
+const kPrimaryLight = Color(0xFFEBEFF3);
+const kPrimaryDark = Color(0xFF141616);
+
+const kSecondaryOrangeDark = Color(0xFFE14A00);
+const kSecondaryLightDark = Color(0xFFF5F8FA);
+const kSecondaryDarkDark = Color(0xFF0A0C0C);
+
+const kSecondaryOrangeNormal = Color(0xFFFF6900);
+const kSecondaryLightNormal = Color(0xFFE1E5E9);
+const kSecondaryDarkNormal = Color(0xFF232828);
+
+const kSecondaryOrangeLight = Color(0xFFFF7D00);
+const kSecondaryLightLight = Color(0xFFD7DBDF);
+const kSecondaryDarkLight = Color(0xFF323838);
+
+const kDangerBackground = Color(0xFFF2DEDE);
+const kDangerTextColor = Color(0xFFB94A48);
+
+const kInfoBackground = Color(0xFFD9EDF7);
+const kInfoTextColor = Color(0xFF3A87AD);
